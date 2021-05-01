3b9ebb5e367686b57a314d9732a06bb579f44367
Mateo <mtaycruz@gmail.com>
2021-05-01 18:32:32 -0400

    authorization middleware connected

--- server/server.js
+++ server/server.js
@@ -5,10 +5,11 @@ const express = require('express');
 
 //import apollo server
 const { ApolloServer } = require('apollo-server-express');
-
+const { authMiddleware } = require('./utils/auth');
 //import our typeDefs and REsolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,7 +17,8 @@ const PORT = process.env.PORT || 3001;
 //create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 //integrate our new Apollo server with the express application as middleware
