a305f3f63625c513626baf3db42224e0c1675da4
Mateo <mtaycruz@gmail.com>
2021-05-02 19:48:19 -0400

    client side connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 // const db = require('./config/connection');
 // const routes = require('./routes');
 
@@ -31,6 +31,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // app.use(routes);
 
